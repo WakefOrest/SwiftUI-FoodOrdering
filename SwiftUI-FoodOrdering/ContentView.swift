277fce31167071f9284f95d590e2b8687fa1d7f1
WakefOrest <senlin.qiu@gmail.com>
2021-05-24 18:02:17 +0800

    Initial Commit

---
+++ SwiftUI-FoodOrdering/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-FoodOrdering
+//
+//  Created by 邱森林 on 2021/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
